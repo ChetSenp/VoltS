734e4cc321ef115d9d6c9931f5be5cf886a6c04d
ChetSenp <np.korp@gmail.com>
2021-08-21 18:27:51 +0300

    ..

---
+++ konstrukt/js/dist/konstrukt.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var allRadios = document.getElementsByName('check');
+var booRadio;
+var x = 0;
+
+for (x = 0; x < allRadios.length; x++) {
+  allRadios[x].onclick = function () {
+    if (booRadio == this) {
+      this.checked = false;
+      booRadio = null;
+    } else {
+      booRadio = this;
+    }
+  };
+}
